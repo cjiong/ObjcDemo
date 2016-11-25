3210ffa588b15c98a3866db35ed6a637bb9d1704
cjiong <416137318@qq.com>
2016-11-25 23:17:36 +0800

    create Project 02 - CustomFonts

---
+++ Project 02 - CustomFonts/Project 02 - CustomFontsTests/Project_02___CustomFontsTests.m
@@ -0,0 +1,39 @@
+//
+//  Project_02___CustomFontsTests.m
+//  Project 02 - CustomFontsTests
+//
+//  Created by 陈炯 on 2016/11/25.
+//  Copyright © 2016年 Jiong. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Project_02___CustomFontsTests : XCTestCase
+
+@end
+
+@implementation Project_02___CustomFontsTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
